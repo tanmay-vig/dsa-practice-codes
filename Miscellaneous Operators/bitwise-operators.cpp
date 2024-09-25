af45b9edfde7716ca63b437b7011d6b7370c7d4b
Tanmay Vig <110380506+tanmay-vig@users.noreply.github.com>
2024-09-25 19:06:30 +0000

    all micellaneous data types added

--- Miscellaneous Operators/bitwise-operators.cpp
+++ Miscellaneous Operators/bitwise-operators.cpp
@@ -4,10 +4,11 @@ using namespace std;
 int main()
 {
     int n = 4 , m = 8;
-    cout << "Bitwise & " << (n & m) << endl
-    << "Biwise | " << (n | m) << endl 
-    << "Biwise ^ " << (n ^m) << endl
-    << "Bitwise << for n=4 " << (n << 1) << endl
-    << "Bitwise >> for n=4 " << (n>>1) << endl;
+    cout << "n = " << n << " m = " << m << endl; 
+    cout << "Bitwise & (and)" << (n & m) << endl
+    << "Biwise | (or)" << (n | m) << endl 
+    << "Biwise ^ (xor)" << (n ^m) << endl
+    << "Bitwise << (left shift) for n=4 " << (n << 1) << endl
+    << "Bitwise >>(right shift) for n=4 " << (n>>1) << endl;
     return 0;
 }
---
+++ Miscellaneous Operators/data-type-modifiers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Size of int " << sizeof(int) << endl <<
+    "Size of long int " << sizeof(long int) << endl <<
+    "Size of short int " << sizeof(short int) << endl<<
+    "Size of long long int " << sizeof(long long int) << endl;
+    return 0;
+}

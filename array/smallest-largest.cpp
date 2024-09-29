193c1110df139886656d3d3e7d4854da15d89ae2
Tanmay Vig <110380506+tanmay-vig@users.noreply.github.com>
2024-09-29 18:55:29 +0000

    all array elements

--- array/smallest-largest.cpp
+++ array/smallest-largest.cpp
@@ -7,6 +7,12 @@ int main()
     int nums[] = {5, 15, 22, 1, 24 , -4, -2,9 };
     int smallest = INT_MAX;
     int maximum = INT_MIN;
+    cout << "Printing the array: " << endl;
+    for (int i = 0; i < sizeof(nums)/sizeof(int); i++)
+    {
+        cout << nums[i] << " ";
+    }
+    cout <<  endl;
     for (int i = 0; i < sizeof(nums)/sizeof(int); i++)
     {
         // if (nums[i] < smallest)

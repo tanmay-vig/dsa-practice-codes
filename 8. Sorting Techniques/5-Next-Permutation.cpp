ba2a5a55e30234a6d0c3d48bb66b5945b31f9926
Tanmay Vig <110380506+tanmay-vig@users.noreply.github.com>
2025-01-19 09:58:10 +0000

    added next permutation

---
+++ 8. Sorting Techniques/5-Next-Permutation.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int pivot = -1;
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                pivot = i;
+                break;
+            }
+        }
+
+        if (pivot == -1) {
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+
+        for (int i = n - 1; i > pivot; i--) {
+            if (nums[i] > nums[pivot]) {
+                swap(nums[i], nums[pivot]);
+                break;
+            }
+        }
+
+        reverse(nums.begin() + pivot + 1, nums.end());
+    }
+};
+int main ()
+{
+    
+    return 0;
+}

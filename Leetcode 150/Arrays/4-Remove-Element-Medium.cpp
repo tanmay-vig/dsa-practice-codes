07637a67094d88511ca6683162172bd85429180d
Tanmay Vig <110380506+tanmay-vig@users.noreply.github.com>
2025-01-20 17:21:44 +0000

    Remove element Medium ques

---
+++ Leetcode 150/Arrays/4-Remove-Element-Medium.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int k = 2;
+
+        if (nums.size() <= 2) return nums.size();
+        
+        for (int i = 2; i < nums.size(); i++) {
+            if (nums[i] != nums[k - 2]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;       
+    }
+};
+int main ()
+{
+    
+    return 0;
+}

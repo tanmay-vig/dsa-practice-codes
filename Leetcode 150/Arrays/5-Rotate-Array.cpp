6a1ed9669ed3146b8c769063ee8c0ca11253bf4f
Tanmay Vig <110380506+tanmay-vig@users.noreply.github.com>
2025-01-21 09:51:22 +0000

    rotate array ques

---
+++ Leetcode 150/Arrays/5-Rotate-Array.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        k=k%nums.size();
+        reverse(nums.begin(),nums.begin()+(nums.size()-k));
+
+        reverse(nums.begin()+(nums.size()-k),nums.end());
+    
+        reverse(nums.begin(),nums.end());
+            
+    }
+};
+
+int main ()
+{
+    
+    return 0;
+}

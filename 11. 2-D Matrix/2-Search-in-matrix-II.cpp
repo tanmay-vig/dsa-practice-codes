b7df4928c16b3c443eb06956ad46f586659ae68f
Tanmay Vig <110380506+tanmay-vig@users.noreply.github.com>
2025-01-31 11:36:22 +0000

    added 2 D matrix ques

---
+++ 11. 2-D Matrix/2-Search-in-matrix-II.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& mat, int target) {
+        int m = mat.size() , n = mat[0].size();
+
+        int r = 0 , c =n-1;
+        while (r < m && c >= 0)
+        {
+            if (target == mat[r][c]) return true;
+            else if (target < mat[r][c]) c--;
+            else r++;
+        }
+
+        return false; 
+    }
+};
+
+int main ()
+{
+    
+    return 0;
+}

5a6aae90472ddbd9362e846783422c3563a952f8
Tanmay Vig <110380506+tanmay-vig@users.noreply.github.com>
2025-02-02 05:17:47 +0000

    two sum , duplicate no. ques added

---
+++ 12. Hashing ques/3-Find-Duplicate-Number.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0] , fast = nums[0];
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        slow = nums[0];
+        while (slow != fast)
+        {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+};
+
+
+int main ()
+{
+    
+    return 0;
+}

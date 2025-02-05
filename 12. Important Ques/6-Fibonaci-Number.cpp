701b02c9b7875c459130eff020e6c9b0ed7281cd
Tanmay Vig <110380506+tanmay-vig@users.noreply.github.com>
2025-02-05 17:50:03 +0000

    fibonanaci number

---
+++ 12. Important Ques/6-Fibonaci-Number.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int fib(int n) {
+        if (n == 0 || n==1)
+            return n;
+        int prev1 = 1, prev2 = 0;
+
+        for (int i = 2; i <= n; i++) {
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+};
+
+int main ()
+{
+    
+    return 0;
+}

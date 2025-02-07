29c62b3c4d3cce8aa6d78c088a4ab4d4052901d1
Tanmay Vig <110380506+tanmay-vig@users.noreply.github.com>
2025-02-07 17:53:30 +0000

    leetcode problem 46

---
+++ 13. Recursion Important/6-Permutations.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector <vector <int>> ans;
+        sort(nums.begin() , nums.end());
+        do{
+            ans.push_back(nums);
+        } while (next_permutation(nums.begin() , nums.end()));
+
+        return ans;
+    }
+};
+
+int main ()
+{
+    
+    return 0;
+}

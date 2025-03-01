39b3dac3376ef06d030208f7a9f5d57f273e734e
Tanmay Vig <110380506+tanmay-vig@users.noreply.github.com>
2025-03-01 09:12:53 +0000

    linked list cycle and merge ques added

---
+++ 14. Linked List/4. Linked-list-Cycle-II.cpp
@@ -0,0 +1,47 @@
+
+
+#include <iostream>
+using namespace std;
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+class Solution
+{
+public:
+    ListNode *detectCycle(ListNode *head)
+    {
+        ListNode *slow = head;
+        ListNode *fast = head;
+        bool isCycle = false;
+        while (fast != NULL && fast->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast)
+            {
+                isCycle = true;
+                break;
+            }
+        }
+        if (!isCycle)
+        {
+            return NULL;
+        }
+        slow = head;
+        while (slow != fast)
+        {
+            slow = slow->next;
+            fast = fast->next;
+        }
+        return slow;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}

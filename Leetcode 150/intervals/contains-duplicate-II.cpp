9f1fb0a1ed8e0eded315e57d476f2447e4dfe63f
Tanmay Vig <110380506+tanmay-vig@users.noreply.github.com>
2025-03-21 08:50:14 +0000

    contains duplicate II added

---
+++ Leetcode 150/intervals/contains-duplicate-II.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+    public:
+        bool containsNearbyDuplicate(vector<int>& nums, int k) {
+            unordered_map<int, int> seen;
+    
+            for (int i = 0; i < nums.size(); i++) {
+                int val = nums[i];
+                if (seen.find(val) != seen.end() && i - seen[val] <= k) {
+                    return true;
+                }
+                seen[val] = i;
+            }
+    
+            return false;
+        }
+    };
+    
+int main ()
+{
+    
+    return 0;
+}

460546185062414b84ce219ff4fde25deb273e0d
Tanmay Vig <110380506+tanmay-vig@users.noreply.github.com>
2025-07-21 16:41:27 +0000

    added all traversals

--- 16. Trees/1-basics.cpp
+++ 16. Trees/1-basics.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
 
 class Node{
@@ -28,11 +29,76 @@ Node* buildTree(vector <int> preorder)
     return root;
 }
 
+// Preorder Traversal 
+void preOrder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    cout << root->data << " ";
+    preOrder(root->left);
+    preOrder(root->right);
+
+}
+
+// Inorder Traversal 
+void inOrder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    inOrder(root->left);
+    cout << root->data << " ";
+    inOrder(root->right);
+
+}
+
+// Post Traversal
+void postOrder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    postOrder(root->left);
+    postOrder(root->right);
+    cout << root->data << " ";
+
+}
+
+// Level Order Traversal
+void levelOrder(Node *root)
+{
+    queue <Node*> q;
+    q.push(root);
+    while (q.size() > 0)
+    {
+        Node *curr = q.front();
+        q.pop();
+        cout << curr->data << " ";
+        if (curr->left != NULL)
+        {
+            q.push(curr->left);
+        }
+        if (curr->right != NULL)
+        {
+            q.push(curr->right);
+        }
+    }
+    cout << endl;
+}
+
 int main ()
 {
     vector <int> preorder = {1 , 2 , -1 , -1 , 3 , 4, -1 , -1 , 5, -1 , -1};
 
     Node *root = buildTree(preorder);
-    cout << root->data << " ";
+    // Traversals 
+    preOrder(root); cout << endl;
+    inOrder(root); cout << endl;
+    postOrder(root); cout << endl;
+    levelOrder(root);
     return 0;
 }
